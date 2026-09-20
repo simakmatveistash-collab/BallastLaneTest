af5b238d04d07efec7ca6a8fbe60da5ee68a67a0
Matvei Simak <matvei.simak@softwarecountry.com>
2026-09-20 10:50:37 -0300

    bug and buldle fixes

--- BallastLaneTest.AppHost/AppHost.cs
+++ BallastLaneTest.AppHost/AppHost.cs
@@ -2,6 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var server = builder.AddProject<Projects.BallastLaneTest_Server>("server")
     .WithHttpHealthCheck("/health")
+    .WithHttpEndpoint(port: 5000)
     .WithExternalHttpEndpoints();
 
 var webfrontend = builder.AddViteApp("webfrontend", "../frontend")
